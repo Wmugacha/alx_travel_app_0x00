c2c48a6a40ee06e1be6c4ff0f70d1f21ef893006
Wmugacha <wilfredmugacha@gmail.com>
2025-06-01 15:24:37 +0800

    migrations done

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -10,7 +10,6 @@ class Listing(models.Model):
     location = models.CharField(max_length=255)
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
     host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings')
-    image = models.ImageField(upload_to='listing_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
